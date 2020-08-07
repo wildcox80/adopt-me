4e13b184e2bd20031711a8d118a77718d5e81481
Wilds Rincon <wildsrincon.developer@gmail.com>
2020-08-06 20:37:36 -0500

    Adding files prettierrc and eslintrc

---
+++ src/App.js
@@ -0,0 +1,30 @@
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", { id: "something-important" }, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cocktaile",
+    }),
+    React.createElement(Pet, {
+      name: "Millie",
+      animal: "Cat",
+      breed: "Mixed",
+    }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));

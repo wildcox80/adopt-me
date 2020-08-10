17e5bfd549ef340b44068d11d3e58409fd30689b
Wilder Rincon <1588@holbertonschool.com>
2020-08-10 01:05:34 -0500

    Adding component search params

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,15 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ANIMALS } from '@frontendmasters/pet';
 
 const SearchParams = () => {
-    const location = "Seatle, WA";
+    const [location, setLocation] = useState("Seatle, WA");
+    const [animal, setAnimal] = useState("Dog");
+    const [breed, setBreed] = useState("Havanese");
+    const [breeds, setBreeds] = useState([]);
 
     return (
         <div className="search-params">
             <form>
                 <label htmlFor="location">
-                    location
-                    <input id="location" value={location}
-                    placeholder="Location" />
+                    Location
+                    <input 
+                        id="location" 
+                        value={location}
+                        placeholder="Location"
+                        onChange={e => setLocation(e.target.value)}
+                    />
+                </label>
+                <label htmlFor="animal">
+                    Animal
+                    <select
+                        id="animal"
+                        value={animal}
+                        onChange={e => setAnimal(e.target.value)}
+                        onBlur={e => setAnimal(e.target.value)}    
+                    >
+                    <option>All</option>
+                    {ANIMALS.map(animal => (
+                        <option key={animal} value={animal}>
+                            {animal}
+                        </option>
+                    ))}
+                    </select>
+                </label>
+                <label htmlFor="breed">
+                    Breed
+                    <select
+                        id="breed"
+                        value={breed}
+                        onChange={e => setBreed(e.target.value)}
+                        onBlur={e => setBreed(e.target.value)}
+                        disabled={breeds.lenght === 0}
+                    >
+                    <option>All</option>
+                    {breeds.map(breedString => (
+                        <option key={breedString} value={breedString}>
+                            {breedString}
+                        </option>
+                    ))}
+                    </select>            
                 </label>
                 <button>Submit</button>
             </form>

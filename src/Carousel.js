2df614decb1b8958fc7b0ab33da1baac62cfbb73
Wilder Rincon <1588@holbertonschool.com>
2020-08-26 20:46:20 -0500

    Adding detail route and carousel component

--- src/Carousel.js
+++ src/Carousel.js
@@ -15,6 +15,12 @@ class Carousel extends React.Component {
     return { photos };
   }
 
+  handleIndexClick = (event) => {
+      this.setState({
+          active: +event.target.dataset.index
+      })
+  }
+
   render() {
     const { photos, active } = this.state;
     return (
@@ -22,8 +28,11 @@ class Carousel extends React.Component {
         <img src={photos[active]} alt="animal" />
         <div className="carousel-smaller">
           {photos.map((photo, index) => (
+              // eslint-disable-next-line
             <img
               key={photo}
+              onClick={this.handleIndexClick}
+              data-index={index}
               src={photo}
               className={index === active ? "active" : ""}
               alt="animal thumbnail"

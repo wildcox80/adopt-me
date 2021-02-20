8a030927fa9c40eaae8e9a2d21a95294dc70c621
Wilder Rincon <1588@holbertonschool.com>
2021-02-20 12:13:24 -0500

    add solution error promises

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -5,78 +5,72 @@ import Results from "./Results";
 import ThemeContext from "./ThemeContext";
 
 const SearchParams = () => {
-    const [theme, setTheme] = useContext(ThemeContext);
-    const [location, updateLocation] = useState("Seattle, WA");
-    const [breeds, setBreeds] = useState([]);
-    const [pets, setPets] = useState([]);
-    const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-    const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [theme, setTheme] = useContext(ThemeContext);
+  const [location, updateLocation] = useState("Seattle, WA");
+  const [breeds, updateBreeds] = useState([]);
+  const [pets, setPets] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown, updateBreed] = useDropdown("Breed", "", breeds);
 
-    async function requestPets() {
-        const { animals } = await pet.animals({
-            location,
-            breed,
-            type: animal
-        });
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    });
 
-        setPets(animals || []);
-    }
+    console.log("animals", animals);
 
-    useEffect(() => {
-        setBreeds([]);
-        setBreed("");
+    setPets(animals || []);
+  }
 
-        pet.breeds(animal)
-            .then(({ breeds }) => {
-                const breedStrings = breeds.map(({ name }) => name);
-                setBreeds(breedStrings);
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-    }, [animal]);
+  useEffect(() => {
+    updateBreeds([]);
+    updateBreed("");
 
-    return ( <
-        div className = "search-params" >
-        <
-        form onSubmit = {
-            e => {
-                e.preventDefault();
-                requestPets();
-            }
-        } >
-        <
-        label htmlFor = "location" >
-        Location <
-        input id = "location"
-        value = { location }
-        placeholder = "Location"
-        onChange = { e => updateLocation(e.target.value) }
-        /> <
-        /label> <
-        AnimalDropdown / >
-        <
-        BreedDropdown / >
-        <
-        label htmlFor = "location" >
-        Theme <
-        select value = { theme }
-        onChange = { e => setTheme(e.target.value) }
-        onBlur = { e => setTheme(e.target.value) } >
-        <
-        option value = "peru" > Peru < /option> <
-        option value = "darkblue" > Dark Blue < /option> <
-        option value = "chartreuse" > Chartreuse < /option> <
-        option value = "mediumorchid" > Medium Orchid < /option> <
-        /select> <
-        /label> <
-        button style = {
-            { backgroundColor: theme } } > Submit < /button> <
-        /form> <
-        Results pets = { pets }
-        /> <
-        /div>
-    );
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      updateBreeds(breedStrings);
+    }, console.error);
+  }, [animal]);
+
+  return (
+    <div className="search-params">
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={e => updateLocation(e.target.value)}
+          />
+        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+        <label htmlFor="location">
+          Theme
+          <select
+            value={theme}
+            onChange={e => setTheme(e.target.value)}
+            onBlur={e => setTheme(e.target.value)}
+          >
+            <option value="peru">Peru</option>
+            <option value="darkblue">Dark Blue</option>
+            <option value="chartreuse">Chartreuse</option>
+            <option value="mediumorchid">Medium Orchid</option>
+          </select>
+        </label>
+        <button style={{ backgroundColor: theme }}>Submit</button>
+      </form>
+      <Results pets={pets} />
+    </div>
+  );
 };
 
 export default SearchParams;
